eccb11494fedc9370699cdc749064a014521993e
darshan245 <48706490+darshan245@users.noreply.github.com>
2022-09-23 21:12:29 +0530

    Add files via upload
    Added pass and fail testcases for ovl_change, ovl_cycle_sequence checkers

---
+++ ivl_uvm_tests/ivl_uvm_ovl_tests/ivl_uvm_ovl_change/ivl_uvm_ovl_change_fail.sv
@@ -0,0 +1,65 @@
+`timescale 1ns/1ns
+
+module test;
+
+   wire clk;
+   reg rst_n;
+   logic sync;
+   logic out;
+
+   ovl_change u_chk_start_event ( 
+			     .clock     (clk),
+			     .reset     (rst_n), 
+			     .enable    (1'b1),
+			     .start_event(sync),
+			     .test_expr (out)
+			     );
+
+      initial begin
+      // Dump waves
+      $dumpfile("dump.vcd");
+      $dumpvars(1, test);
+
+      // Initialize values.
+      rst_n = 1;
+      sync  = 0;
+      out   = 0;
+   
+      $display("ovl_change does not fire at rst_n");
+      rst_n = 0;
+      wait_clks(10);
+      $display("Out of reset");
+
+      sync = 0;
+      out  = 1;
+      $display("checking test_expr(out = %0d) without any delay",out);
+
+      sync = 1;
+      wait_clks(3);
+      out  = 1;
+      $display("checking test_expr(out = %0d) when wait_clk is 1",out);
+      
+      rst_n = 1;
+      sync = 1;
+      wait_clks(5);
+      out  = 0;
+      $display("checking test_expr(out = %0d) when wait_clk is 5",out);
+     
+      rst_n = 1;
+      sync = 1;
+      wait_clks(10);
+      out  = 1;
+      $display("checking test_expr(out = %0d) when wait_clk is 10",out);
+
+      $finish;
+   end
+
+   task wait_clks (input int num_clks = 3);
+      repeat (num_clks) @(posedge clk);
+   endtask : wait_clks
+
+  ivl_uvm_ovl_clk_gen #(.FREQ_IN_MHZ(100)) u_clk_100 (clk);
+
+endmodule
+
+
---
+++ ivl_uvm_tests/ivl_uvm_ovl_tests/ivl_uvm_ovl_cycle_sequence/ivl_uvm_ovl_cycle_sequence_pass.sv
@@ -0,0 +1,61 @@
+`timescale 1ns/1ns
+
+module test;
+
+   wire clk;
+   reg rst_n;
+   logic [3:0] data;
+
+   // Check wr_val is not same as wr_done
+   ovl_cycle_sequence #(.num_cks(4)) u_chk_ovl_cycle_sequence ( 
+			     .clock     (clk),
+			     .reset     (rst_n), 
+			     .enable    (1'b1),
+			     .event_sequence (data)
+			     );
+
+   initial begin
+      // Dump waves
+      $dumpfile("dump.vcd");
+      $dumpvars(1, test);
+
+      // Initialize values.
+      rst_n = 0;
+      data  = 4'b0000;
+
+      wait_clks(3);
+      rst_n = 1;
+      wait_clks(2);
+      data  = 4'b1000;
+      $display("checking event_sequence(data = %0b) after 2 clk cycles",data);
+
+      wait_clks(2);
+      data  = 4'b0100;
+      $display("checking event_sequence(data = %0b) after 2 clk cycles",data);
+
+      wait_clks(2);
+      data  = 4'b0010;
+      $display("checking event_sequence(data = %0b) after 2 clk cycles",data);
+
+      wait_clks(2);
+      data  = 4'b0001;
+      $display("checking event_sequence(data = %0b) after 2 clk cycles",data);
+
+      /*alw_high_sig = 1;
+      $display({"ovl_always does not fire ",
+                "when alw_high_sig is FALSE"});
+
+      wait_clks(10);*/
+
+      $finish;
+   end
+
+   task wait_clks (input int num_clks = 1);
+      repeat (num_clks) @(posedge clk);
+   endtask : wait_clks
+
+  ivl_uvm_ovl_clk_gen #(.FREQ_IN_MHZ(100)) u_clk_100 (clk);
+
+endmodule
+
+
